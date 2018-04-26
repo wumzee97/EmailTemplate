952a923eed9bb725e27e958ec5d20262552cf391
wumzee97 <38761326+wumzee97@users.noreply.github.com>
2018-04-26 16:13:55 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,219 @@
+<html>
+<head>
+	
+	<title>Dream Secure Email</title>
+
+	<style type="text/css">
+
+body{
+	font-family: "museo 300", verdana;
+	background-color: #1a1a1a;
+}
+.main{
+	background-color: #fff;
+	width: 550px;
+	height: 1060px;
+	//padding: 2%;
+	border: 1px solid #000;
+	margin: 0 auto;
+}
+
+.banner{
+	//background: linear-gradient(to right, #ffffff, #cf1700);
+	width: 550px;
+	height: 90px;
+	border-bottom: 3px solid #cf1700;
+	padding-bottom: 3px;
+	text-align: center;
+}
+
+.logo{
+	height: 80;
+	margin: 10px;
+	float: left;
+}
+
+.myhead{
+	float: left;
+	padding-left: 40px;
+	padding-top: 15px;
+	text-align: center;
+}
+
+.menu{
+	float: right;
+	font-size: 10pt;
+	padding-top: 25px;
+	padding-right: 10px;
+}
+
+.single-image{
+	height: 300px;
+	width: 550px;
+	
+	
+}
+
+.message-box{
+	width: 490px;
+	font-size: 11pt;
+	margin: 15px;
+	float: left;
+	text-align: justify;
+
+	padding-left: 15px;
+	padding-right: 30px;
+	
+}
+
+.download-btn{
+	width: 150px;
+}
+
+.footer{
+	width: 550px;
+	height: 150px;
+	background-color: #F6A087;
+	float: left;
+	border-top: 3px solid #cf1700;
+	padding-top: 10px;
+}
+
+.round{
+	color: #cf1700;
+	padding: 5px;
+	border: 2px dashed #cf1700;
+	font-weight: bolder;
+}
+
+p a{
+	text-decoration: none;
+	color: #000;
+}
+
+p a:hover{
+	color: #cf1700;
+}
+
+.button a{
+	text-decoration: none;
+	color: #fff;
+}
+
+.button a:hover{
+	color: #fff;
+}
+
+.button {
+  background-color: #cf1700;
+  -webkit-border-radius: 10px;
+  border-radius: 10px;
+  border: none;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: inline-block;
+  
+  font-size: 20px;
+  padding: 5px 10px;
+  text-align: center;
+  text-decoration: none;
+}
+@-webkit-keyframes glowing {
+  0% { background-color: #B20000; -webkit-box-shadow: 0 0 3px #B20000; }
+  50% { background-color: #FF0000; -webkit-box-shadow: 0 0 20px #FF0000; }
+  80% { background-color: #B20000; -webkit-box-shadow: 0 0 3px #B20000; }
+}
+
+@-moz-keyframes glowing {
+  0% { background-color: #B20000; -moz-box-shadow: 0 0 3px #B20000; }
+  50% { background-color: #FF0000; -moz-box-shadow: 0 0 20px #FF0000; }
+  80% { background-color: #B20000; -moz-box-shadow: 0 0 3px #B20000; }
+}
+
+@-o-keyframes glowing {
+  0% { background-color: #B20000; box-shadow: 0 0 3px #B20000; }
+  50% { background-color: #FF0000; box-shadow: 0 0 20px #FF0000; }
+  80% { background-color: #B20000; box-shadow: 0 0 3px #B20000; }
+}
+
+@keyframes glowing {
+  0% { background-color: #B20000; box-shadow: 0 0 3px #B20000; }
+  50% { background-color: #FF0000; box-shadow: 0 0 20px #FF0000; }
+  80% { background-color: #B20000; box-shadow: 0 0 3px #B20000; }
+}
+
+.button {
+  -webkit-animation: glowing 1500ms infinite;
+  -moz-animation: glowing 1500ms infinite;
+  -o-animation: glowing 1500ms infinite;
+  animation: glowing 1500ms infinite;
+}
+</style>
+</head>
+	<body style="font-family: "museo 300", verdana;	background-color: #1a1a1a;" >
+			<div class="main" style="background-color: #fff; width: 550px; height: 1365px; border: 1px solid #000; margin: 0 auto;">
+				<div class="banner" style="width: 550px; height: 90px; border-bottom: 3px solid #cf1700; text-align: center; ">
+					<img src="https://dreammesh.ng/dsadvert_mail/img/logo.png" class="logo" style="height: 50px; margin: 10px; margin-left: 30px; margin-top: 20px; float: left;" />
+					<!-- <h2 class="myhead" style="float: left; padding-left: 40px; padding-top: 15px; text-align: center;">Dream Secure</h2> -->
+					<div class="menu" style="float: right; font-size: 10pt; padding-top: 25px; padding-right: 10px;">
+						<p><a href="https://dreammesh.ng/dreamsecure" style="text-decoration: none; color: #000;">ABOUT</a> |	<a href="https://dreammesh.ng/dreamsecure/#Download" style="text-decoration: none; color: #000;">SUPPORT</a> |	<a href="https://dreammesh.ng/dreamsecure/#Pricing" style="text-decoration: none; color: #000;">DOWNLOAD</a></p>
+					</div>
+				</div>
+			<!-- 	<br/> -->
+				<div class="single-image" style="height: 300px;	width: 550px;">
+				<img style="float: left;" src="https://dreammesh.ng/dsadvert_mail/img/Dream-secure-Email.gif">
+				</div>
+
+				<div class="message-box" style="width: 490px; font-size: 11pt; margin: 15px; float: left; text-align: justify; padding-left: 15px; padding-right: 30px;">
+				<!-- <p>The goal is to make Nigeria safer, one person at a time.</p> -->
+<p>Have you seen the news today?! It’s riddled with tons of criminal acts all around the
+country. Kidnap, armed robbery, rape, murder and a long list of offences against
+humanity that makes us want to scream <span style="font-weight: bold; text-transform: italics;">Nigeria needs to be saved!</span></p>
+
+<p>We are really not here to talk about the security problem in Nigeria, No! We are
+here to proffer a solution to help keep you and your loved ones safe; A personal
+Safety App that is totally FREE!</p>
+<p>Dream Secure is a mobile App that alerts your family/friends as well as emergency
+agencies whenever you sense that you are imminent danger.</p>
+
+
+<br/>
+<center>
+<a href="http://ios.me/app/1301962346/dream-secure"><img src="https://dreammesh.ng/dsadvert_mail/img/ios.png" class="download-btn" style="width: 150px;" /></a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="https://play.google.com/store/apps/details?id=com.dreammesh.dreamsecure"><img src="https://dreammesh.ng/dsadvert_mail/img/android.png" class="download-btn" style="width: 150px;" /></a> <br/><br/>
+<p class="button" style="background-color: #cf1700; -webkit-border-radius: 10px; border-radius: 10px; border: none; color: #FFFFFF; cursor: pointer; display: inline-block; font-size: 20px; padding: 5px 10px; text-align: center; text-decoration: none;"><a href="https://dreammesh.ng/dreamsecure" style="color: #fff; text-decoration: none;">Read more about Dream Secure here</a></p> </center>
+<p style="font-weight: bolder;">Forward this newsletter and spread the news to keep your loved ones safe!</p>
+
+<br/>
+
+<!-- App Available on IOS and Android -->
+<center>
+				</div>
+
+				<div class="single-image" style="height: 300px;	width: 550px; float: left;">
+					<a href="https://www.youtube.com/watch?v=TGLD9f0cxNs"><img src="https://dreammesh.ng/dsadvert_mail/img/dss.PNG" height="300px" width="550px"></a>
+                       
+				</div>
+
+				<div class="footer" style="width: 550px; height: 150px;	background-color: #E6E9EB; float: left;	border-top: 3px solid #cf1700; padding-top: 10px;">
+				<!-- <p style="font-size: 8pt;">Powered by: Dream Mesh Limited</p> -->
+					<center>
+					<br/>
+						<a href="http://dreammesh.ng"><img src="https://dreammesh.ng/dsadvert_mail/img/dreammesh.png" width="120px" /></a> <br/> <br/>
+						<a href="https://www.facebook.com/dreammeshltd"><img src="https://dreammesh.ng/dsadvert_mail/img/fb-link.png" width="40px" /></a>
+						<a href="https://twitter.com/dreammeshltd"><img src="https://dreammesh.ng/dsadvert_mail/img/tt-link.png" width="40px" /></a>
+						<a href="https://plus.google.com/s/dreammeshltd/top"><img src="https://dreammesh.ng/dsadvert_mail/img/gp-link.png" width="40px" /></a>
+						<a href="https://www.instagram.com/dreammeshltd/"><img src="https://dreammesh.ng/dsadvert_mail/img/ist.png" width="40px" /></a>
+						<a href="https://www.linkedin.com/company/dream-mesh-ltd/"><img src="https://dreammesh.ng/dsadvert_mail/img/link.png" width="40px" /></a>
+						<a href="https://www.youtube.com/channel/UCkTHPdlzmJ9IozTevgD5UHg"><img src="https://dreammesh.ng/dsadvert_mail/img/youtb.png" width="40px" /></a>
+
+						
+					</center>
+
+				</div>
+
+
+
+			</div>
+
+	</body>
+<html>
